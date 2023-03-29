9a41d7b1fffaa3aa59db3eb65d317b867690bf89
Amir Mohammad Hl <dev.imorate@gmail.com>
2023-03-29 13:31:11 +0430

    Initial commit

---
+++ src/main/java/ir/imorate/imotodo/ImoTodoApplication.java
@@ -0,0 +1,13 @@
+package ir.imorate.imotodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoTodoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoTodoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/imorate/imotodo/ImoTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.imorate.imotodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
